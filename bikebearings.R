b0e3879bd9e4c072b7aa066fc693ed973a83dafd
echristiansen2019 <46330701+echristiansen2019@users.noreply.github.com>
2019-12-07 15:50:31 -0500

    added faceting option for radar graph

--- bikebearings.R
+++ bikebearings.R
@@ -1,6 +1,6 @@
 ##bearings
 
-bikebearing <- function(bdata, round = TRUE, samestation = TRUE, output = c("vector", "plot")){
+bikebearing <- function(bdata, round = TRUE, samestation = TRUE, output = c("vector", "plot"), facet_group = "NULL"){
   
   library(fossil)
   library(dplyr)
@@ -31,20 +31,53 @@ bikebearing <- function(bdata, round = TRUE, samestation = TRUE, output = c("vec
   #return either vector or radar plot
   if (output == "vector"){
     return(bearings)
-  } else if (output == "plot"){
+  } 
+  
+  ##returning plot
+  else if (output == "plot"){
     
     library(ggplot2)
     
+    ##aggregate for plotting
     
-    plotdat <- as.data.frame(table(bearings))
-    plotdat$bearings <- as.factor(plotdat$bearings)
+    ##split if faceting chart 
     
-    g <- ggplot(plotdat, aes(x = bearings, y = Freq, group = 1)) +
-      geom_polygon() + coord_polar() + theme_bw() + scale_x_discrete(breaks = c(0, 90, 180, 270)) +
-      scale_y_continuous(labels = scales::comma)+
-      labs(x = "", y = "Count of Rides")
-    
-    return(g)
+    ##no faceting
+    if (is.null(facet_group)){
+      plotdat <- cbind(b, bearings)
+      
+      ##aggregate just on bearing count
+      plotdat <- aggregate(Bike.number ~ bearings, plotdat, length)
+      names(plotdat) <- c("bearings", "ride_count")
+      
+      g <- ggplot(plotdat, aes(x = bearings, y = ride_count, group = 1)) +
+        geom_polygon() + coord_polar() + theme_bw() + scale_x_discrete(breaks = c(0, 90, 180, 270)) +
+        scale_y_continuous(labels = scales::comma)+
+        labs(x = "", y = "Count of Rides")
+      
+      return(g) 
+      
+    } else{
+      
+      plotdat <- cbind(b, bearings)
+      #build both sides of aggregation equation
+      lhs = "Bike.number ~"
+      rhs = paste0("bearings +", facet_group)
+      
+      #aggregate on bearings and facet group
+      plotdat <- aggregate(as.formula(paste(lhs,rhs)), plotdat, length)
+      names(plotdat) <- c("bearings", facet_group, "ride_count")
+      
+      
+      g <- ggplot(plotdat, aes_string(x = 'bearings', y = 'ride_count', group = facet_group)) +
+        geom_polygon() + coord_polar() + theme_bw() + scale_x_discrete(breaks = c(0, 90, 180, 270)) +
+        scale_y_continuous(labels = scales::comma)+
+        labs(x = "", y = "Count of Rides") + facet_wrap(facet_group)
+      
+      return(g) 
+      
+    }
+   
     
   }
   
